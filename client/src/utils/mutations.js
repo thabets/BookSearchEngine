4788dca76f627c3c9707e6cc1362255173c6c2c8
Sam Thabet <sallam.thabet@gmail.com>
2021-11-13 22:08:14 -0500

    added code for app.js added mutation for client added queries for client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,44 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($id: ID!) {
+    saveBook(bookId: $id) {
+      _id
+      description
+      title
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(bookId: $id) {
+      _id
+      description
+      title
+    }
+  }
+`;
